fbccb083e2e63de677c78f95bf56171ca9b7dfc0
talismo.admin <seamoss@talismo.io>
2022-12-11 18:04:37 -0800

    prior to migration FROM vite to webpack. Installed mesh

--- vite.config.ts
+++ vite.config.ts
@@ -12,3 +12,23 @@ export default defineConfig({
     }
   }
 })
+
+//mesh webpack copy
+/* not working
+module.exports = {
+  resolve: {
+    fallback: {
+      buffer: require.resolve("buffer"),
+      stream: require.resolve("stream"),
+    },
+  },
+  plugins: [
+    new webpack.ProvidePlugin({
+      Buffer: ["buffer", "Buffer"],
+    }),
+  ],
+  experiments: {
+    asyncWebAssembly: true,
+  },
+};
+*/
